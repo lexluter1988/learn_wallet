b60d52be8ad05fc19b75dd0d298f34f43c11e421
Alexey Suponin <lexxsmith@gmail.com>
2016-08-07 01:12:55 +0300

    fixed inheritance

--- accounts.py
+++ accounts.py
@@ -1,66 +1,47 @@
-# import cPickle as pickle
 from datetime import datetime
 
 
 class Account(object):
     '''Base class for all accounts'''
 
-    def __init__(self, clean=False):
-        self.clean = clean
+    def __init__(self):
         self.description = 'Base Account Class - not for use!'
-        self.__last_id = 0
-        self.__last_updated = ''
-        self.__value = 0
-        self.__in_sync = False
+        self._last_id = 0
+        self._last_updated = ''
+        self._value = 0
+        self._in_sync = False
 
     def clear(self):
-        if self.clean:
-            self.__last_id = 0
-            self.__value = 0
-            self.__in_sync = True
+        self._last_id = 0
+        self._value = 0
+        self._in_sync = True
 
     @property
     def last_id(self):
-        return self.__last_id
+        return self._last_id
 
     @property
     def last_updated(self):
-        return self.__last_updated
+        return self._last_updated
 
     @property
     def in_sync(self):
-        return self.__in_sync
+        return self._in_sync
 
     def synchronize(self):
-        self.__last_updated = datetime.now().strftime("%Y-%m-%d %H:%M")
-        self.__last_id += 1
-        self.__in_sync = True
+        self._last_updated = datetime.now().strftime("%Y-%m-%d %H:%M")
+        self._last_id += 1
+        self._in_sync = True
 
     @property
     def value(self):
-        return self.__value
+        return self._value
 
     @value.setter
     def value(self, value):
-        self.__value += value
+        self._value += value
         self.synchronize()
         return True
-
-    # def get_balance(self):
-    #     # need try cause we have EOFError exception
-    #     try:
-    #         f = None
-    #         f = open('account.pickle', 'rb')
-    #         self.temp = pickle.load(f)
-
-    #     except IOError:
-    #         pass
-    #     except EOFError:
-    #         pass
-
-    #     finally:
-    #         if f:
-    #             f.close()
 
     def __repr__(self):
         text = super(Account, self).__repr__()
@@ -70,28 +51,32 @@ class Account(object):
 class Cash(Account):
     '''Just cash account where paper money are stored'''
 
-    def __init__(self, clean=False):
-        self.description = 'Cash Account'
+    def __init__(self):
+        super(Cash, self).__init__()
+        self.description = 'Cash'
 
 
 class Credit(Account):
     '''Credit account where credit card balance stored'''
 
-    def __init__(self, clean=False):
-        self.description = 'Credit Account'
+    def __init__(self):
+        super(Credit, self).__init__()
+        self.description = 'Credit'
         self.rate = 0
 
 
 class Debit(Account):
     '''Debit account where salary card balance is stored'''
 
-    def __init__(self, clean=False):
-        self.description = 'Debit Account'
+    def __init__(self):
+        super(Debit, self).__init__()
+        self.description = 'Debit'
 
 
 class Savings(Account):
     '''Savings account where $ and other savings are stored'''
 
-    def __init__(self, clean=False):
-        self.description = 'Savings Account'
+    def __init__(self):
+        super(Savings, self).__init__()
+        self.description = 'Savings'
         self.currency = 'RUB'
