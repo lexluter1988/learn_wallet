1b53b75e3f3d3c08159a13cfa3cb24e0a15e0f47
Alexey Suponin <lexxsmith@gmail.com>
2016-08-06 15:51:40 +0300

    add stdout logger

--- controls.py
+++ controls.py
@@ -1,4 +1,36 @@
 import logging
+import sys
+
+
+class CmdLog(type):
+
+    def __new__(cls, name, bases, namespace, **kwargs):
+        result = type.__new__(cls, name, bases, dict(namespace))
+        # changing logger to strdout
+        out_handler = logging.StreamHandler(sys.stdout)
+        # format for output
+
+        out_handler.setFormatter(logging.Formatter(
+                '%(asctime)s [%(levelname)s] [%(name)s] - %(message)s'))
+        out_handler.setLevel(logging.DEBUG)
+
+        # adjust out logger
+        result.logger = logging.getLogger(name)
+        result.logger.addHandler(out_handler)
+        result.logger.setLevel(logging.DEBUG)
+
+        return result
+
+
+class CheckMeta(object):
+    __metaclass__ = CmdLog
+
+    def __init__(self, value):
+        self.value = value
+        self.logger.debug("testing logger, value is {0}".format(value))
+
+    def show_logs(self):
+        self.logger.info("hello child, you said {0}".format(self.value))
 
 
 class CmdMux(object):
@@ -11,21 +43,6 @@ class CmdMux(object):
 
     def __init__(self):
         pass
-
-
-class CmdLog(type):
-
-    def __new__(cls, name, bases, namespace, **kwargs):
-        result = type.__new__(cls, name, bases, dict(namespace))
-        result.logger = logging.getLogger(name)
-        return result
-
-
-class CheckMeta(object):
-    __metaclass__ = CmdLog
-
-    def __init__(self, value):
-        self.logger.info("testing logger, value is {0}".format(value))
 
 
 class OutDevice(object):
