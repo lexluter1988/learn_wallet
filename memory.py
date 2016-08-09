dedacf11346b3cd05e41a310f2bf5761d6f17fc9
Alexey Suponin <lexxsmith@gmail.com>
2016-08-09 17:41:24 +0300

    fixes #4 #5 #6 #7: implemented memory classes

--- memory.py
+++ memory.py
@@ -5,13 +5,17 @@ class BalanceHistory(object):
     '''This is imutable memory unit, containing balance history'''
 
     def __init__(self):
-        pass
+        self.balance_records = []
 
-    def put_record(self):
-        pass
+    def put_record(self, record):
+        if isinstance(record, BalanceRecord):
+            self.balance_records.append(record)
+            return True
+        else:
+            return False
 
-    def get_record(self):
-        pass
+    def get_record(self, records_range):
+        return self.balance_records[:records_range]
 # You can select the latest protocol with the -1 argument.
 # if we use slots and pickle, we got
 # TypeError: a class that defines __slots__ without
@@ -33,13 +37,13 @@ class BalanceRecord(object):
     '''This is just record ready to be stored in history'''
     __slots__ = ['id', 'date', 'cash', 'debit', 'credit', 'savings']
 
-    def __init__(self):
+    def __init__(self, cash, debit, credit, savings):
         self.id = 0
         self.date = datetime.now().strftime("%Y-%m-%d %H:%M")
-        self.cash = 0
-        self.debit = 0
-        self.credit = 0
-        self.savings = 0
+        self.cash = cash
+        self.debit = debit
+        self.credit = credit
+        self.savings = savings
 
 
 class PayHistory(object):
@@ -51,26 +55,31 @@ class PayHistory(object):
         # will the list be empty?
         self.pay_records = []
 
+        # oh wait, we will assign here values from our file
+
     # we generate payment object in cmd mux
     # then we send it to payhistory object
     # who loads everything
     def put_payment(self, payment):
-        self.pay_records.append(payment)
-        return True
+        if isinstance(payment, PayRecord):
+            self.pay_records.append(payment)
+            return True
+        else:
+            return False
 
     # getting record is also simple
     # by sending id as how much we want to see
-    def get_payment(self, records_range):
-        return self.pay_records[:records_range]
+    def get_payment(self, payments_range):
+        return self.pay_records[:payments_range]
 
 
 class PayRecord(object):
     '''Class ready to be stored in history of Payments Incomes'''
     __slots__ = ['id', 'date', 'category', 'value', 'comment']
 
-    def __init__(self):
+    def __init__(self, category, value, comment):
         self.id = 0
         self.date = datetime.now().strftime("%Y-%m-%d %H:%M")
-        self.category = 0
-        self.value = 0
-        self.comment = ''
+        self.category = category
+        self.value = value
+        self.comment = comment
